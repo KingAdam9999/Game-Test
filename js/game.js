670adabd34fde39f4c8957e60192514d9d4cc859
KingAdam9999 <adameffendy99@gmail.com>
2026-08-28 11:51:25 +0800

    Implement Game class with basic structure

--- js/game.js
+++ js/game.js
@@ -1,1 +1,50 @@
+export class Game {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext("2d");
 
+        this.running = false;
+        this.lastTime = 0;
+    }
+
+    start() {
+        this.running = true;
+
+        this.resizeCanvas();
+        window.addEventListener("resize", () => this.resizeCanvas());
+
+        requestAnimationFrame((time) => this.gameLoop(time));
+    }
+
+    resizeCanvas() {
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+    }
+
+    gameLoop(currentTime) {
+        if (!this.running) return;
+
+        const deltaTime = (currentTime - this.lastTime) / 1000;
+        this.lastTime = currentTime;
+
+        this.update(deltaTime);
+        this.draw();
+
+        requestAnimationFrame((time) => this.gameLoop(time));
+    }
+
+    update(deltaTime) {
+        // Game logic will go here.
+    }
+
+    draw() {
+        this.ctx.clearRect(
+            0,
+            0,
+            this.canvas.width,
+            this.canvas.height
+        );
+
+        // Drawing will go here.
+    }
+}
