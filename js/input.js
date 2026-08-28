a6b638e2e5ae9f6c80defe17bd331307ae064a69
KingAdam9999 <adameffendy99@gmail.com>
2026-08-28 11:52:41 +0800

    Add Input class for keyboard event handling

--- js/input.js
+++ js/input.js
@@ -1,1 +1,17 @@
+export class Input {
+    constructor() {
+        this.keys = new Set();
 
+        window.addEventListener("keydown", (event) => {
+            this.keys.add(event.key.toLowerCase());
+        });
+
+        window.addEventListener("keyup", (event) => {
+            this.keys.delete(event.key.toLowerCase());
+        });
+    }
+
+    isKeyDown(key) {
+        return this.keys.has(key.toLowerCase());
+    }
+}
